79d2b80593dd29782038d1118e91bac7fd5b3a40
shortpoet <soriano.carlitos@gmail.com>
2020-03-14 15:14:19 -0500

    initial commit

---
+++ Shortpoet/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shortpoet.Data;
+using Shortpoet.Models;
+
+[assembly: HostingStartup(typeof(Shortpoet.Areas.Identity.IdentityHostingStartup))]
+namespace Shortpoet.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
