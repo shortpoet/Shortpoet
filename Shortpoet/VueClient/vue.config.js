735fd61c0302c99d0dd01d628fecd484f18ff9c0
shortpoet <soriano.carlitos@gmail.com>
2020-03-20 13:21:19 -0500

    added some commented settings to vue.config for potential future use, colors mapping file, and trying to add a . in favicon path in index.html to correct bug in deployed

--- Shortpoet/VueClient/vue.config.js
+++ Shortpoet/VueClient/vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
     configureWebpack: {
       devtool: 'source-map'
     },
+    // baseUrl: process.env.NODE_ENV === 'production' ? '/subpath/' : '/',
     // publicPath: process.env.NODE_ENV === 'production'
     //   ? '/production-sub-path/'
     //   : '/',
@@ -21,6 +22,16 @@ module.exports = {
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
         "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
       }
+      // http://vuejs-templates.github.io/webpack/static.html
+      // https://stackoverflow.com/questions/43879418/deploy-vuejs-app-in-a-subdirectory/43880261#43880261
+      // https://cli.vuejs.org/guide/html-and-static-assets.html
+      // Other options include:
+      // outputDir: undefined,
+      // assetsDir: undefined,
+      // runtimeCompiler: undefined,
+      // productionSourceMap: undefined,
+      // parallel: undefined,
+      // css: undefined
     }
   }
   
