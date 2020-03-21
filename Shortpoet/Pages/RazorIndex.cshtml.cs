b9a5348c68eadfb77f54c799b42d520304874e00
shortpoet <soriano.carlitos@gmail.com>
2020-03-20 20:54:25 -0500

    changed routing for resume to be /

--- Shortpoet/Pages/RazorIndex.cshtml.cs
+++ Shortpoet/Pages/RazorIndex.cshtml.cs
@@ -8,11 +8,11 @@ using Microsoft.Extensions.Logging;
 
 namespace Shortpoet.Pages
 {
-    public class IndexModel : PageModel
+    public class RazorIndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ILogger<RazorIndexModel> _logger;
         public string Message { get; set; }
-        public IndexModel(ILogger<IndexModel> logger)
+        public RazorIndexModel(ILogger<RazorIndexModel> logger)
         {
             _logger = logger;
         }
