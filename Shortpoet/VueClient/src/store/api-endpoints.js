11e0f3e4d012b91eb728b1bbe49d5ee3675a20c2
shortpoet <soriano.carlitos@gmail.com>
2020-04-03 20:41:59 -0500

    fixed href for api endpoint

--- Shortpoet/VueClient/src/store/api-endpoints.js
+++ Shortpoet/VueClient/src/store/api-endpoints.js
@@ -1,24 +1,20 @@
 export const index = {
   // ROOT STORE @/store/index.js
-  // BACKEND_PREFIX_PROD: 'https://tableau-interface.apps.pcfpre-phx.cloud.boeing.com',
   // TODO
-  //     let clientRoot = window.location.protocol + '//' + window.location.host
-  // FROM dbana/app/.../authservice.js
-
+  // let clientRoot = window.location.protocol + '//' + window.location.host
   // BACKEND_PREFIX_PROD: 'https://' + window.location.hostname,
   BACKEND_PREFIX_PROD: window.location.href,
-  BACKEND_PREFIX_DEV: 'https://localhost:5001'
+  BACKEND_PREFIX_DEV: 'https://localhost:5001/'
 }
 export const resume = {
   // RESUME @/store/modules/StoreResume.js
-  RESUME_GET_API: '/api/Resume/Get',
-  RESUME_FETCH_API: '/api/Resume/Fetch/1'
+  RESUME_GET_API: 'api/Resume/Get',
+  RESUME_FETCH_API: 'api/Resume/Fetch/1'
 }
 export const auth = {
   // AUTH @/store/modules/StoreAuth.js
-  ROLES_API: '/api/Roles',
-  HEADERS_API: '/api/Idy/Get',
-  BOEING_USER_API: 'https://insite.web.boeing.com/culture/service/boeingUserWebServiceJSON/bemsid?query='
+  ROLES_API: 'api/Roles',
+  HEADERS_API: 'api/Idy/Get'
 }
 export const endpoints = {
   index: index,
