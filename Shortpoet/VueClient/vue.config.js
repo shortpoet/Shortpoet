07ba5defad44b0b957873ec10e6237e41b35a382
shortpoet <soriano.carlitos@gmail.com>
2020-04-22 14:47:22 -0500

    added rocket icon and ripple animation

--- Shortpoet/VueClient/vue.config.js
+++ Shortpoet/VueClient/vue.config.js
@@ -28,7 +28,7 @@ module.exports = {
     // adding public seemed to do the same as
     // adding the below cmd to package.json
     // "servelocal": "vue-cli-service serve --host localhost",
-    // public: 'https://localhost:8080/',
+    public: 'https://localhost:8080/',
     // port: 8080, // CHANGE YOUR PORT HERE!
     https: true,
     // hotOnly: false,
