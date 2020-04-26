684455ab978f01f4849e6d28f3f8e853c046204a
shortpoet <soriano.carlitos@gmail.com>
2020-04-26 17:32:17 -0500

    retesting with no conditional in add resume

--- Shortpoet/Data/AddResume.cs
+++ Shortpoet/Data/AddResume.cs
@@ -15,22 +15,22 @@ namespace Shortpoet.Data
     public static void Seed(ResumeDbContext context, IWebHostEnvironment environment)
     {
 
-      Boolean writeJson = true;
+      // Boolean writeJson = true;
 
-      // AddItems(context, EducationJson.LoadJson("Data/Seed/carlos_resume_educations.json").Educations);
-      // AddItems(context, JobJson.LoadJson("Data/Seed/carlos_resume_jobs.json").Jobs);
-      // context.Add(Resume.LoadJson("Data/Seed/carlos_resume_about.json"));
-      // AddItems(context, SkillJson.LoadJson("Data/Seed/carlos_resume_skills.json").Skills);
-      // AddItems(context, SocialJson.LoadJson("Data/Seed/carlos_resume_socials.json").Socials);
-      // AddItems(context, LanguageJson.LoadJson("Data/Seed/carlos_resume_languages.json").SpokenLanguages);
-      // context.SaveChanges();
+      AddItems(context, EducationJson.LoadJson("Data/Seed/carlos_resume_educations.json").Educations);
+      AddItems(context, JobJson.LoadJson("Data/Seed/carlos_resume_jobs.json").Jobs);
+      context.Add(Resume.LoadJson("Data/Seed/carlos_resume_about.json"));
+      AddItems(context, SkillJson.LoadJson("Data/Seed/carlos_resume_skills.json").Skills);
+      AddItems(context, SocialJson.LoadJson("Data/Seed/carlos_resume_socials.json").Socials);
+      AddItems(context, LanguageJson.LoadJson("Data/Seed/carlos_resume_languages.json").SpokenLanguages);
+      context.SaveChanges();
       
-      // AddItems(context, ResumeEducationsJson.LoadJson("Data/Seed/carlos_resume_resumeeducations.json").ResumeEducations);
-      // AddItems(context, ResumeJobsJson.LoadJson("Data/Seed/carlos_resume_resumejobs.json").ResumeJobs);
-      // AddItems(context, ResumeSkillsJson.LoadJson("Data/Seed/carlos_resume_resumeskills.json").ResumeSkills);
-      // AddItems(context, ResumeSocialsJson.LoadJson("Data/Seed/carlos_resume_resumesocials.json").ResumeSocials);
-      // AddItems(context, ResumeSpokenLanguagesJson.LoadJson("Data/Seed/carlos_resume_resumespokenlanguages.json").ResumeSpokenLanguages);
-      // context.SaveChanges();
+      AddItems(context, ResumeEducationsJson.LoadJson("Data/Seed/carlos_resume_resumeeducations.json").ResumeEducations);
+      AddItems(context, ResumeJobsJson.LoadJson("Data/Seed/carlos_resume_resumejobs.json").ResumeJobs);
+      AddItems(context, ResumeSkillsJson.LoadJson("Data/Seed/carlos_resume_resumeskills.json").ResumeSkills);
+      AddItems(context, ResumeSocialsJson.LoadJson("Data/Seed/carlos_resume_resumesocials.json").ResumeSocials);
+      AddItems(context, ResumeSpokenLanguagesJson.LoadJson("Data/Seed/carlos_resume_resumespokenlanguages.json").ResumeSpokenLanguages);
+      context.SaveChanges();
 
         // AddItems(context, EducationJson.LoadEducations("Data/Seed/carlos_resume_educations.yml", writeJson).Educations);
         // AddItems(context, JobJson.LoadJobs("Data/Seed/carlos_resume_jobs.yml", writeJson).Jobs);
@@ -56,42 +56,42 @@ namespace Shortpoet.Data
       // // resume = Resume.YmlToResume(path);
       // context.Add(resume);
       
-      if (environment.EnvironmentName == "Development")
-      {
-        AddItems(context, EducationJson.LoadEducations("Data/Seed/carlos_resume_educations.yml", writeJson).Educations);
-        AddItems(context, JobJson.LoadJobs("Data/Seed/carlos_resume_jobs.yml", writeJson).Jobs);
-        context.Add(Resume.LoadResume("Data/Seed/carlos_resume_about.yml", writeJson));
-        AddItems(context, SkillJson.LoadSkills("Data/Seed/carlos_resume_skills.yml", writeJson).Skills);
-        AddItems(context, SocialJson.LoadSocials("Data/Seed/carlos_resume_socials.yml", writeJson).Socials);
-        AddItems(context, LanguageJson.LoadLanguages("Data/Seed/carlos_resume_languages.yml", writeJson).SpokenLanguages);
-        context.SaveChanges();
+      // if (environment.EnvironmentName == "Development")
+      // {
+      //   AddItems(context, EducationJson.LoadEducations("Data/Seed/carlos_resume_educations.yml", writeJson).Educations);
+      //   AddItems(context, JobJson.LoadJobs("Data/Seed/carlos_resume_jobs.yml", writeJson).Jobs);
+      //   context.Add(Resume.LoadResume("Data/Seed/carlos_resume_about.yml", writeJson));
+      //   AddItems(context, SkillJson.LoadSkills("Data/Seed/carlos_resume_skills.yml", writeJson).Skills);
+      //   AddItems(context, SocialJson.LoadSocials("Data/Seed/carlos_resume_socials.yml", writeJson).Socials);
+      //   AddItems(context, LanguageJson.LoadLanguages("Data/Seed/carlos_resume_languages.yml", writeJson).SpokenLanguages);
+      //   context.SaveChanges();
         
-        AddItems(context, ResumeEducationsJson.LoadResumeEducations("Data/Seed/carlos_resume_resumeeducations.yml", writeJson).ResumeEducations);
-        AddItems(context, ResumeJobsJson.LoadResumeJobs("Data/Seed/carlos_resume_resumejobs.yml", writeJson).ResumeJobs);
-        AddItems(context, ResumeSkillsJson.LoadResumeSkills("Data/Seed/carlos_resume_resumeskills.yml", writeJson).ResumeSkills);
-        AddItems(context, ResumeSocialsJson.LoadResumeSocials("Data/Seed/carlos_resume_resumesocials.yml", writeJson).ResumeSocials);
-        AddItems(context, ResumeSpokenLanguagesJson.LoadResumeSpokenLanguages("Data/Seed/carlos_resume_resumespokenlanguages.yml", writeJson).ResumeSpokenLanguages);
-        context.SaveChanges();
+      //   AddItems(context, ResumeEducationsJson.LoadResumeEducations("Data/Seed/carlos_resume_resumeeducations.yml", writeJson).ResumeEducations);
+      //   AddItems(context, ResumeJobsJson.LoadResumeJobs("Data/Seed/carlos_resume_resumejobs.yml", writeJson).ResumeJobs);
+      //   AddItems(context, ResumeSkillsJson.LoadResumeSkills("Data/Seed/carlos_resume_resumeskills.yml", writeJson).ResumeSkills);
+      //   AddItems(context, ResumeSocialsJson.LoadResumeSocials("Data/Seed/carlos_resume_resumesocials.yml", writeJson).ResumeSocials);
+      //   AddItems(context, ResumeSpokenLanguagesJson.LoadResumeSpokenLanguages("Data/Seed/carlos_resume_resumespokenlanguages.yml", writeJson).ResumeSpokenLanguages);
+      //   context.SaveChanges();
 
-      }
-      else 
-      {
-        AddItems(context, EducationJson.LoadJson("Data/Seed/carlos_resume_educations.json").Educations);
-        AddItems(context, JobJson.LoadJson("Data/Seed/carlos_resume_jobs.json").Jobs);
-        context.Add(Resume.LoadJson("Data/Seed/carlos_resume_about.json"));
-        AddItems(context, SkillJson.LoadJson("Data/Seed/carlos_resume_skills.json").Skills);
-        AddItems(context, SocialJson.LoadJson("Data/Seed/carlos_resume_socials.json").Socials);
-        AddItems(context, LanguageJson.LoadJson("Data/Seed/carlos_resume_languages.json").SpokenLanguages);
-        context.SaveChanges();
+      // }
+      // else 
+      // {
+      //   AddItems(context, EducationJson.LoadJson("Data/Seed/carlos_resume_educations.json").Educations);
+      //   AddItems(context, JobJson.LoadJson("Data/Seed/carlos_resume_jobs.json").Jobs);
+      //   context.Add(Resume.LoadJson("Data/Seed/carlos_resume_about.json"));
+      //   AddItems(context, SkillJson.LoadJson("Data/Seed/carlos_resume_skills.json").Skills);
+      //   AddItems(context, SocialJson.LoadJson("Data/Seed/carlos_resume_socials.json").Socials);
+      //   AddItems(context, LanguageJson.LoadJson("Data/Seed/carlos_resume_languages.json").SpokenLanguages);
+      //   context.SaveChanges();
         
-        AddItems(context, ResumeEducationsJson.LoadJson("Data/Seed/carlos_resume_resumeeducations.json").ResumeEducations);
-        AddItems(context, ResumeJobsJson.LoadJson("Data/Seed/carlos_resume_resumejobs.json").ResumeJobs);
-        AddItems(context, ResumeSkillsJson.LoadJson("Data/Seed/carlos_resume_resumeskills.json").ResumeSkills);
-        AddItems(context, ResumeSocialsJson.LoadJson("Data/Seed/carlos_resume_resumesocials.json").ResumeSocials);
-        AddItems(context, ResumeSpokenLanguagesJson.LoadJson("Data/Seed/carlos_resume_resumespokenlanguages.json").ResumeSpokenLanguages);
-        context.SaveChanges();
+      //   AddItems(context, ResumeEducationsJson.LoadJson("Data/Seed/carlos_resume_resumeeducations.json").ResumeEducations);
+      //   AddItems(context, ResumeJobsJson.LoadJson("Data/Seed/carlos_resume_resumejobs.json").ResumeJobs);
+      //   AddItems(context, ResumeSkillsJson.LoadJson("Data/Seed/carlos_resume_resumeskills.json").ResumeSkills);
+      //   AddItems(context, ResumeSocialsJson.LoadJson("Data/Seed/carlos_resume_resumesocials.json").ResumeSocials);
+      //   AddItems(context, ResumeSpokenLanguagesJson.LoadJson("Data/Seed/carlos_resume_resumespokenlanguages.json").ResumeSpokenLanguages);
+      //   context.SaveChanges();
 
-      }
+      // }
 
       
     }
