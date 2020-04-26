91458e4398d6fa24aa66ae548d68bea93bad88aa
shortpoet <soriano.carlitos@gmail.com>
2020-04-26 13:34:21 -0500

    fixed migration namespace issue

--- Shortpoet/Migrations/ResumeDbContextModelSnapshot.cs
+++ Shortpoet/Migrations/ResumeDbContextModelSnapshot.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shortpoet.Data;
 
-namespace Shortpoet.MigrationsTest
+namespace Shortpoet.Migrations
 {
     [DbContext(typeof(ResumeDbContext))]
     partial class ResumeDbContextModelSnapshot : ModelSnapshot
