dd3af78d8e6ccb69837cb9bc986206d440ae9a8d
shortpoet <soriano.carlitos@gmail.com>
2020-05-11 22:13:38 -0500

    test works

--- Shortpoet/VueClient/tests/unit/pdf/pdf.pdfButtonFloat.spec.js
+++ Shortpoet/VueClient/tests/unit/pdf/pdf.pdfButtonFloat.spec.js
@@ -238,6 +238,7 @@ describe("pdf.pdfButtonFloat", () => {
         allowTaint: true,
       };
 
+      // https://github.com/hustcc/jest-canvas-mock
       const getDataURL = jest.fn(
         () =>
           "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
