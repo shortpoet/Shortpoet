6da9ccef35dd0a42a8477b90c5b7d7f75d0a2f5f
shortpoet <soriano.carlitos@gmail.com>
2020-06-04 10:25:22 -0500

    layout tweaks

--- Shortpoet/VueClient/src/views/PDF.vue
+++ Shortpoet/VueClient/src/views/PDF.vue
@@ -61,14 +61,11 @@
         :educations="getResume.educations"
       />
       <div id="interests-poem-container" class="row mb-5">
-        <div id="interests-container" class="col-12 col-md-8">
+        <div id="interests-container" class="col-12">
           <PDFInterests
             :interests="getResume.interests"
             :renderPDF="renderPDF"
           />
-        </div>
-        <div id="poem-container" class="col-12 col-md-4 mt-3 mt-md-0 mr-0 text-center">
-          <img class="img-fluid img-poem rounded" :src="pic" alt="poem" :style="poemStyle">
         </div>
       </div>
 
