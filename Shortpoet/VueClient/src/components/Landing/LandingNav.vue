aad1227727800ed47905226803dcf4e725260fdb
shortpoet <soriano.carlitos@gmail.com>
2020-06-05 08:08:59 -0500

    change landing buttons to green, both same style, purple on hover

--- Shortpoet/VueClient/src/components/Landing/LandingNav.vue
+++ Shortpoet/VueClient/src/components/Landing/LandingNav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="landing-nav-container d-flex flex-row justify-content-across mt-3 mt-lg-0 pl-3 p-lg-5 mr-lg-0 mr-5">
-    <button class="bowton landing-button-1" @click="showResume">Resume</button>
-    <router-link tag="button" to="/blog/learning-to-unit-test-en" class="bowton landing-button-2 mx-5">Blog</router-link>
+    <button class="bowton landing-button" @click="showResume">Resume</button>
+    <router-link tag="button" to="/blog/learning-to-unit-test-en" class="bowton landing-button mx-5">Blog</router-link>
   </div>
 </template>
 
