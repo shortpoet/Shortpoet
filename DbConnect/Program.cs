43dfa32dabde0e88c8e1f5ad513a7371503efc9a
shortpoet <soriano.carlitos@gmail.com>
2020-12-09 12:08:11 -0600

    resolve discrepancies btw local,test,prod

--- DbConnect/Program.cs
+++ DbConnect/Program.cs
@@ -22,12 +22,12 @@ namespace DbConnect
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
             var Configuration = builder.Build();
 
-            // string connStr = Configuration.GetConnectionString("localClient");
-            string connStr = Configuration.GetConnectionString("localClientTest");
+            string connStr = Configuration.GetConnectionString("localClient");
+            // string connStr = Configuration.GetConnectionString("localClientTest");
             // string connStr = Configuration.GetConnectionString("spClientTestAzure");
             // string connStr = Configuration.GetConnectionString("spClientProdAzure");
 
-            Console.WriteLine(connStr);
+              // Console.WriteLine(connStr);
             
             string schema = "Profiles";
             string table = "Resumes";
@@ -38,8 +38,10 @@ namespace DbConnect
             // Results.ShowDataTable(results);
 
 
-            string dataFolder = "d20200710";
-            string resumeFolder = "r20200710";
+            string dataFolder = "d20201208";
+            string resumeFolder = "r20201208";
+            // string dataFolder = "d20200710";
+            // string resumeFolder = "r20200710";
 
             ResumeService.AddResumeData(new Data.ResumeDbContext(ContextConfig.GetConfig(connStr)), dataFolder);
             // ResumeService.WriteJson(dataFolder);
